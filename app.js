61f722d0b232e5f1ab39ecb8dd6670faf49ef7b8
hangilJung <ever_g@naver.com>
2022-04-25 10:50:51 +0900

    Add function security

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const app = express();
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const { wrongApproch } = require("./src/lib/middleware");
+const helmet = require("helmet");
+const hpp = require("hpp");
 
 dotenv.config();
 
 const index = require("./src/routes");
 
 app.use(express.json());
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+}
 
 app.use("/api", index);
 
